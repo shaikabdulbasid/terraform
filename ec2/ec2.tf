60ecfc8ff6279c66c97dcd44e407fb6c32799aff
shaikabdulbasid <shaikabdulbasid7@gmail.com>
2024-05-13 19:37:43 +0530

    Commit

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,18 +3,18 @@ resource="aws_security_group" "allow_ssh" {
     description= "allowing SSH access"
 
     ignore{
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
         cidr_block  = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
+        
     }
     egress {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
         cidr_block  = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
+        
     }
     tags{
         Name = "allow_ssh"
